22a4de87dee5a6c4388f7c86c33d73ed7b6d456b
athirakmohn <athira.mail12@gmail.com>
2013-01-29 16:39:31 +0530

    Updated sql

--- vendor.sql
+++ vendor.sql
@@ -1,28 +1,36 @@
 
 
-CREATE DATABASE vendor
 
 
+
+
+DROP DATABASE IF EXISTS `vendor`;
+CREATE DATABASE vendor DEFAULT CHARACTER SET utf8;
+
+USE `vendor`;
+
+DROP TABLE IF EXISTS `vendor`;
 CREATE TABLE vendor(
   id int(15) NOT NULL AUTO_INCREMENT,
   name varchar(30) DEFAULT NULL,
   purchaseOrderAvailable int(1) DEFAULT NULL,
-  purchaseNumber  varchar(40) DEFAULT NULL,
-  orderType int(11) NOT NULL DEFAULT 1,
-  cost int(10),
-  PRIMARY KEY (id)  
+  purchaseNumber varchar(40) DEFAULT NULL,
+  orderType int(11) NOT NULL DEFAULT 1,  
+  PRIMARY KEY (id)
 ) ;
+
+
+DROP TABLE IF EXISTS `order_type`;
 CREATE TABLE order_type (
   id int(11) NOT NULL,
   name varchar(30) DEFAULT NULL,
   PRIMARY KEY (id)
 ) ;
 
-
-
 INSERT INTO vendor.order_type(id,name) VALUES(1,'Staff Augmentation');
 INSERT INTO vendor.order_type(id,name) VALUES(2,'Outbound Projects');
 INSERT INTO vendor.order_type(id,name) VALUES(3,'Software Licensing');
 INSERT INTO vendor.order_type(id,name) VALUES(4,'Hardware Purchase');
+
 
 Alter table vendor add purchaseCost int(11)after purchaseNumber;
